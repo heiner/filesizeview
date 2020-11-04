b5f460c4e6f2378f11e409702aad910335c3e3d2
Heinrich Kuttler <hnr@fb.com>
2020-11-04 01:28:16 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def main():
 
     setuptools.setup(
         name="filesizeview",
-        version="0.0.5",
+        version="0.0.6",
         description="Graphical display of file sizes on terminal",
         long_description=long_description,
         long_description_content_type="text/markdown",
